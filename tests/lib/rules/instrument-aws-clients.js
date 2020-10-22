0599729ddb0b7bd6c46d7a262432a64af4b4e24a
Marcin Jan Puhacz <marcinpuhacz@gmail.com>
2020-10-22 15:48:07 +0200

    feat: extend instrumented-aws-clients rule to support more cases (#16)

--- tests/lib/rules/instrument-aws-clients.js
+++ tests/lib/rules/instrument-aws-clients.js
@@ -15,11 +15,55 @@ var rule = require("../../../lib/rules/instrument-aws-clients"),
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester()
+var ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+})
+
 ruleTester.run("instrument-aws-clients", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    "import SecretsManager from 'aws-sdk/clients/secretsmanager'\n" +
+      "const ssm = new SecretsManager()\n" +
+      "AWSXRay.captureAWSClient(ssm)",
+
+    "import { SecretsManager, DynamoDB } from 'aws-sdk/clients/secretsmanager'\n" +
+      "let ddb = new DynamoDB()\n" +
+      "AWSXRay.captureAWSClient(ddb)\n" +
+      "AWSXRay.captureAWSClient(new SecretsManager())",
   ],
 
-  invalid: [],
+  invalid: [
+    {
+      code:
+        "import SecretsManager from 'aws-sdk/clients/secretsmanager'\n" +
+        "var ssm = new SecretsManager()\n" +
+        "function test() {\n" +
+        "  const ssm = new SecretsManager()\n" +
+        "  AWSXRay.captureAWSClient(ssm)\n" +
+        "}\n" +
+        "ssm.cancelRotateSecret()",
+      errors: [
+        {
+          messageId: "clientNotInstrumented",
+          data: { client: "ssm = new SecretsManager()" },
+        },
+      ],
+    },
+
+    {
+      code:
+        "import SecretsManager from 'aws-sdk/clients/secretsmanager'\n" +
+        "const ssm = new SecretsManager()\n" +
+        "ssm.cancelRotateSecret()\n" +
+        "AWSXRay.captureAWSClient(ssm)",
+      errors: [
+        {
+          messageId: "clientNotInstrumented",
+          data: { client: "ssm = new SecretsManager()" },
+        },
+      ],
+    },
+  ],
 })
